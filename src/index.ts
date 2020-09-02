90c43a8d3dbf45d8664c69e2668c8889873efc6e
Dave Hadka <dhadka@github.com>
2020-09-02 12:31:39 -0500

    Update to gh-stockpile@1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core'
-import * as url from 'url'
-import { AzureStorageConfiguration, AzureStorageClient } from 'gh-stockpile'
+import { Stockpile, Client } from 'gh-stockpile'
 
 async function copy() {
     const src = core.getInput('src')
@@ -8,57 +7,25 @@ async function copy() {
     const ttl = core.getInput('ttl')
     const ifNotFound = core.getInput('if-not-found')
 
-    let accountName: string | undefined = undefined
-    let containerName: string | undefined = undefined
+    let client: Client | undefined = undefined
     let blobName: string | undefined = undefined
     let filePath: string | undefined = undefined
-    let sasToken = process.env["SAS_TOKEN"]
 
-    if (src.startsWith("http://") || src.startsWith("https://")) {
-        const srcUrl = url.parse(src)
-        accountName = srcUrl.host?.substring(0, srcUrl.host.indexOf('.'))
-        containerName = srcUrl.path?.substring(1, srcUrl.path.indexOf('/', 1))
-        blobName = srcUrl.path?.substring(2 + (containerName?.length ?? 0))
+    if (src.indexOf("://")) {
+        client = Stockpile.createClient(src)
+        blobName = Stockpile.getBlobName(src)
         filePath = dest
     }
 
-    if (dest.startsWith("http://") || dest.startsWith("https://")) {
-        const destUrl = url.parse(dest)
-        accountName = destUrl.host?.substring(0, destUrl.host.indexOf('.'))
-        containerName = destUrl.path?.substring(1, destUrl.path.indexOf('/', 1))
-        blobName = destUrl.path?.substring(2 + (containerName?.length ?? 0))
+    if (dest.indexOf("://")) {
+        client = Stockpile.createClient(dest)
+        blobName = Stockpile.getBlobName(dest)
         filePath = src
     }
 
-    core.info(`Account: ${accountName}, Container: ${containerName}, Blob: ${blobName}`)
-
-    if (!accountName) {
-        core.error("Missing account name")
-        return
+    if (!client || !blobName || !filePath) {
+        throw Error(`src or dest must reference a storage account from a supported provider`)
     }
-
-    if (!containerName) {
-        core.error("Missing container name")
-        return
-    }
-
-    if (!blobName) {
-        core.error("Missing blob name")
-        return
-    }
-
-    if (!sasToken) {
-        core.error("Missing SAS token")
-        return
-    }
-
-    if (!filePath) {
-        core.error("Missing local file path")
-        return
-    }
-
-    let configuration = new AzureStorageConfiguration(accountName, containerName, sasToken)
-    let client = new AzureStorageClient(configuration)
 
     try {
         if (filePath === src) {
@@ -70,7 +37,7 @@ async function copy() {
     } catch (error) {
         core.setOutput("successful", "false")
 
-        if (error.response && error.response.statusCode == 404 && ifNotFound === 'skip') {
+        if (error.statusCode && error.statusCode == 404 && ifNotFound === 'skip') {
             return
         }
 
